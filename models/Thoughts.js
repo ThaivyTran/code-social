67e5d045f6f229cef270c1dc6b1e886f1a182fed
ThaivyTran <Tran.thaivy1997@gmail.com>
2023-02-19 15:17:36 -0500

    added models thoughts and users

---
+++ models/Thoughts.js
@@ -0,0 +1,33 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const ReactionsSchema = new Schema(
+    {
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        // moments for dates
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
+
+module.exports = Thoughts;
---
+++ models/Users.js
@@ -0,0 +1,35 @@
+// requires mongoose
+const { Schema, model } = require('mongoose');
+
+const UsersSchema = new Schema(
+    {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thoughts'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false
+    }
+)
+
+module.exports = Users;
